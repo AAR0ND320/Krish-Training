27bda15fd0d6446ed5d9b983a2e9da70768984c8
AAR0ND320 <aaron.dezoysa@gmail.com>
2022-03-01 21:35:28 +0530

    Changed the initializing of the object

--- DesignPatterns/src/builder/Application.java
+++ DesignPatterns/src/builder/Application.java
@@ -5,7 +5,7 @@ public class Application {
 	public static void main(String[] args) {
 		Mobile.Builder mBuilder = new Mobile.Builder(2600);
 		
-		Mobile phone1 = mBuilder.waterproof(false).dropProtection("2yrs").serviceWarranty("1yr").build();
+		Mobile phone1 = mBuilder.waterproof(false).dropProtection("2yrs").build();
 		
 		System.out.println(phone1);
 	}
