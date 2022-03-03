5b6c8dce28a6cc0cd28add3f0c069fb301d0188b
AAR0ND320 <aaron.dezoysa@gmail.com>
2022-03-03 14:35:34 +0530

    Finished the application logic

--- DesignPatterns/src/chainofresponsibility/Application.java
+++ DesignPatterns/src/chainofresponsibility/Application.java
@@ -7,6 +7,14 @@ public class Application {
 		FirstTier firstTier = new FirstTier();
 		SecondTier secondTier = new SecondTier();
 		ThirdTier thirdTier = new ThirdTier();
+		
+		noTier.setSuccessor(firstTier);
+		firstTier.setSuccessor(secondTier);
+		secondTier.setSuccessor(thirdTier);
+		
+		Donation donation = new Donation(1, 25);
+		noTier.provideBenefit(donation);
+		
 	}
 	
 }
