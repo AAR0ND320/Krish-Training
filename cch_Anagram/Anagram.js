b1dd4ac4d80c3c362649738f57acadecfbd7ab55
AAR0ND320 <aaron.dezoysa@gmail.com>
2022-03-07 13:17:29 +0530

    created function to convert words into maps for each letter

--- cch_Anagram/Anagram.js
+++ cch_Anagram/Anagram.js
@@ -1,3 +1,29 @@
-function checkAnagram(word1, word2){
+function checkAnagram(word1, word2) {
+    let first = word1.toUppercase();
+    let second = word2.toUppercase();
+
+    if(first.length != second.length) {
+        return false;
+    }
+
     
+
 }
+
+function mapWordLetters(word) {
+    let letterMap = {};
+
+    for(let letter of word) {
+        if(letterMap.hasOwnProperty(letter)) {
+            // Increment the value in the existing element
+            letterMap[letter] = letterMap[letter] + 1;
+        } else {
+            // Create the new element for the letter
+            letterMap[letter] = 1;
+        }
+    }
+
+    return letterMap;
+}
+
+module.exports.checkAnagram = checkAnagram;
