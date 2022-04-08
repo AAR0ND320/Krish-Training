8f29a192b567e7cb61278ff7f5a42dda33ae13d8
AAR0ND320 <aaron.dezoysa@gmail.com>
2022-04-08 08:11:26 +0530

    Implemented TreeNode inner class.

--- cch_BFS_DFS/src/com/aaron/training/bfs/Application.java
+++ cch_BFS_DFS/src/com/aaron/training/bfs/Application.java
@@ -1,6 +1,13 @@
 package com.aaron.training.bfs;
 
 public class Application {
+	
+	class TreeNode {
+		boolean visited;
+		int data;
+		TreeNode left;
+		TreeNode right;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
